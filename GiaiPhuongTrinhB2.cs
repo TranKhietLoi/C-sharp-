659f1ead1311364103c8fe92d6580c9b2f5b4728
Tran Khiet Loi <trankhietloi@>
2024-07-09 12:59:54 +0700

    Add project files.

---
+++ GiaiPhuongTrinhB2.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class GiaiPhuongTrinhB2
+    {
+
+        //Hàm tính phương trình bậc 2
+        public double delta(int a, int b, int c)
+        {
+            //Viết hàm phân tích trường hợp delta
+            double delta = b*b - 4*a*c;
+
+            return delta;
+
+        }
+        public void CacTruongHop(double delta)
+        {
+            if (delta > 0)
+            {
+                Console.WriteLine("Phương trình có 2 nghiệm phân biệt!");
+            }
+            else if (delta == 0)
+            {
+                Console.WriteLine("Phương trình có nghiệpm kép!");
+            }
+            else
+            {
+                Console.WriteLine("Phương trình vô nghiệm!");
+            }
+        }
+        public Tuple<double,double> TimNghiem(double delta,int a, int b, int c)
+        
+        {
+            double x1, x2;
+            if (delta > 0)
+            {
+                x1 = (-b+Math.Sqrt(delta))/ (2*a);
+                x2 = (-b-Math.Sqrt(delta))/ (2*a);
+                return Tuple.Create(x1, x2);
+
+            }
+            else if(delta == 0)
+            {
+                x1 = (-b)/2*a;
+                x2 = x1; 
+                return Tuple.Create(x1, x2);
+            }
+            return null;
+
+        }
+       
+        public Tuple<int,int,int> InterfaceInput()
+        {
+            Console.WriteLine("Lần lượt nhập vào giá trị a,b,c,d cho phương trình bậc 2: ax+by+c = 0");
+            Console.WriteLine("Nhập vào a:");
+            string _a = Console.ReadLine();
+            int a = Convert.ToInt32(_a);
+
+            Console.WriteLine("Nhập vào b:");
+            string _b = Console.ReadLine();
+            int b = Convert.ToInt32(_b);
+
+            Console.WriteLine("Nhập vào c:");
+            string _c = Console.ReadLine();
+            int c = Convert.ToInt32(_c);
+            return Tuple.Create(a, b, c);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using ConsoleApp23;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Chào mừng bạn đã đến với hệ thống tính toán giải phương trình bậc 2 thông thái!");
+
+            GiaiPhuongTrinhB2 giaiPhuongTrinh = new GiaiPhuongTrinhB2();
+
+            // Gọi phương thức InterfaceInput để nhận giá trị a, b, c
+            var inputValues = giaiPhuongTrinh.InterfaceInput();
+            int a = inputValues.Item1;
+            int b = inputValues.Item2;
+            int c = inputValues.Item3;
+
+            // Tính delta
+            double del = giaiPhuongTrinh.delta(a, b, c);
+
+            // Hiển thị các trường hợp của delta
+            giaiPhuongTrinh.CacTruongHop(del);
+
+            // Tìm nghiệm và hiện thị kết quả
+            var nghiem = giaiPhuongTrinh.TimNghiem(del, a, b, c);
+
+            // Xuất thông báo nghiệm
+            if (nghiem != null)
+            {
+                double x1 = nghiem.Item1;
+                double x2 = nghiem.Item2;
+                Console.WriteLine($"Nghiệm x1: {x1}, Nghiệm x2: {x2}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
